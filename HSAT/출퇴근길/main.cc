befd967496a417dd0a6305e98860c7bf8ab0205f
sehyunyuk <sdfg8931@ajou.ac.kr>
2024-11-10 16:29:18 +0900

    update

---
+++ HSAT/출퇴근길/main.cc
@@ -0,0 +1,159 @@
+			#include<iostream>
+			#include<algorithm>
+			#include<vector>
+			#include<stack>
+			// #define DEBUG
+			using namespace std;
+
+
+			bool visit_list[200001]={0,};
+			bool is_sroop_list[200001]={0,};
+			bool is_troop_list[200001]={0,};
+			int order_list[200001]={0,};
+			int child_idx_list[200001]={0,};
+			vector<int> edge_list[200001];
+			int order =0;
+			int level=0;
+
+
+			bool dfs(int start, bool* is_roop_list) {
+				stack<int> st;  // <노드, 자식 인덱스>
+				st.push(start);
+				while (!st.empty()){
+
+					int current = st.top();
+					int child_idx=child_idx_list[current];
+
+
+					if (order_list[current] == 0) {
+						order++;
+						order_list[current] = order;
+					}
+					#ifdef DEBUG
+					cout << "current : " << current << "child_idx : "<< child_idx<< "order "<< order_list[current]<< endl;
+					#endif
+					// else{
+					// 	order--;
+					// }
+
+					// 현재 노드의 모든 자식을 처리했다면
+					if (child_idx >= edge_list[current].size()) {
+
+					for(int child: edge_list[current]){
+						is_roop_list[current] |=  is_roop_list[child];
+						}
+
+					st.pop();
+
+					visit_list[current] = false;
+					order_list[current]=0;
+					order--;
+
+
+					}
+					else{
+						int next = edge_list[current][child_idx];
+
+						if (is_roop_list[next]) {
+							is_roop_list[current] = true;
+						}
+						else if (!visit_list[next] | (order_list[current] > order_list[next])) {
+						visit_list[next] = true;
+						st.push(next);
+						#ifdef DEBUG
+						cout << "push : " << next << endl;
+						#endif
+						}
+						child_idx_list[current] += 1;
+					}
+				}
+
+				return true;
+			}
+
+			int main(){
+			int N, M;
+			cin >> N >> M;
+
+			for(int i= 0; i<M; i++){
+
+			int start ,end;
+			cin >> start >> end;
+			edge_list[start].push_back(end);
+
+			}
+
+			int S, T;
+			cin >> S >> T;
+
+			order=1;
+
+			for(int i=0; i<200001; i++){
+					order_list[i]=0;
+					visit_list[i]=0;
+					is_sroop_list[i]=0;
+					child_idx_list[i]=0;
+			}
+			order_list[S]=1;
+			is_sroop_list[S] =true;
+			is_sroop_list[T] =true;
+			dfs(S, is_sroop_list );
+
+			#ifdef DEBUG
+			cout <<"first dfs============="<<endl;
+			#endif
+
+
+
+			#ifdef DEBUG
+			cout <<"second dfs============="<<endl;
+			#endif
+
+			for(int i=0; i<200001; i++){
+					order_list[i]=0;
+					visit_list[i]=0;
+					is_troop_list[i]=0;
+					child_idx_list[i]=0;
+			}
+			order=1;
+			is_troop_list[T]=true;
+			is_troop_list[S]=true;
+			order_list[T]=1;
+			dfs(T ,is_troop_list );
+
+
+
+			int result =0;
+
+			for(int i=0; i<200001; i++){
+				if(i != S & i !=T){
+				result += (is_sroop_list[i]&is_troop_list[i]);
+				}
+			}
+
+			#ifdef DEBUG
+
+			for(int i=0; i<20; i++){
+				cout<< is_sroop_list[i] << " ";
+			}
+			cout << endl;
+			for(int i=0; i<20; i++){
+				cout<< is_troop_list[i] << " ";
+			}
+			cout << endl;
+
+			for(int i=0; i<20; i++){
+				cout<< visit_list[i] << " ";
+			}
+			cout << endl;
+
+			for(int i=0; i<20; i++){
+				cout<< visit_list[i] << " ";
+			}
+			cout << endl;
+			#endif
+
+			cout << result;
+
+
+			}
---
+++ HSAT/출퇴근길/make.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+  cout <<  100000 <<" "<< 199998 << endl;
+  for(int i =1; i<100000; i++){
+    cout << i <<" "<< i+1 << endl;
+  }
+    for(int i =100000; i>1; i--){
+    cout << i <<" "<< i-1 << endl;
+  }
+  cout << 1 <<" "<< 100000<<endl;
+
+
+
+
+}
+
