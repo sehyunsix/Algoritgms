1e38622c5d277cc737f2007e994a2d33914ea70a
육세현 <139142064+sehyunsix@users.noreply.github.com>
2024-12-03 14:01:09 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,18 @@
+   #include<iostream>
+   #include<algorithm>
+   #include<vector>
+   #include<memory.h>
+   #include<queue>
+   #include <string>
+   #define ll long long
+   using namespace std;
+
+
+   int main(){
+
+    cout << "\\    /\\\n";   // 첫 번째 줄
+    cout << " )  ( ')\n";  // 두 번째 줄
+    cout << "(  /  )\n";   // 세 번째 줄
+    cout << " \\(__)|\n";   // 네 번째 줄
+    return 0;
+   }
