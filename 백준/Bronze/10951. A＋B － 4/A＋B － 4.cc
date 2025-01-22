18e71fe3dd223d8f195b5aad8aa7b252047494d5
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-01-22 13:15:35 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#include<queue>
+#include <string>
+#include<math.h>
+#define ll long long
+#define P 1000000007
+using namespace std;
+int main() {
+  cin.tie(0);
+cout.tie(0);
+ios_base::sync_with_stdio(false);
+
+int a, b;
+
+
+while (!(cin >> a >> b).eof()) {
+
+  cout << a + b << "\n";
+};
+return 0;
+}
