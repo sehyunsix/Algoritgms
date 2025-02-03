e341d8cfef44bf07ab545c9d506ded7f4048683f
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-03 10:30:44 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#include<queue>
+#include <string>
+#include<math.h>
+#define ll long long
+#define P 1000000007
+#define endl "\n"
+using namespace std;
+int main() {
+cin.tie(0);
+cout.tie(0);
+ios_base::sync_with_stdio(false);
+cout << "Hello World!";
+
+
+return 0;
+}
