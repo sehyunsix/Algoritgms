57f7641ca5266819692e72742d3a54196cf89248
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-04 14:00:24 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#include<queue>
+#include <string>
+#include<math.h>
+#include<map>
+#define ll long long
+#define P 1000000007
+#define endl "\n"
+using namespace std;
+int main() {
+  cin.tie(0);
+cout.tie(0);
+ios_base::sync_with_stdio(false);
+int N,M;
+cin >> N>>M;
+vector<int> numbers(N);
+vector<int> accum(N + 1, 0);
+for (int i = 0; i < N; i++) {
+  cin >> numbers[i];
+}
+
+for (int i = 0;i < N;i++) {
+  accum[i + 1] = accum[i] + numbers[i];
+}
+
+for (int i = 0; i < M; i++) {
+  int st, en;
+  cin >> st >> en;
+  cout << accum[en] - accum[st-1] << endl;
+
+}
+
+
+return 0;
+}
