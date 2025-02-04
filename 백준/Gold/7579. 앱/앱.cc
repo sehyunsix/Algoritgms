ea5995197a145edcee45e3c324733deb22104c11
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-05 00:17:38 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<algorithm>
+#define ll long long
 using namespace std;
-
 int N, M, ans, sum;
 int bite[101], cost[101];
-int dp[101][10001];
+
 
 int main()
 {
   cin >> N >> M;
-
+  vector<ll>dp(10001, 0);
   for (int i = 1; i <= N; i++) cin >> bite[i];
   for (int i = 1; i <= N; i++)
   {
@@ -18,22 +18,20 @@ int main()
   }
 
   // dp[i][j] == i번째 앱까지 탐색했을 때
-  // j비용을 소모해서 얻을 수 있는 최대 메모리 
+  // j비용을 소모해서 얻을 수 있는 최대 메모리
+    dp[0] = 0;
 
-  for (int i = 1; i <= N; i++)
-  {
-    for (int j = 0; j <= sum; j++)
-    {
-      if (j - cost[i] >= 0)
-        dp[i][j] = max(dp[i][j], dp[i - 1][j - cost[i]] + bite[i]);
-      dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+  for (int i = 1; i <= N;i++) {
+    for (int j =sum; j >= 0;j--) {
+      if (j - cost[i] >= 0) {
+          dp[j] = max(dp[j - cost[i]] + bite[i], dp[j]);
+        }
+      }
     }
-  }
-
 
   for (int i = 0; i <= sum; i++)
   {
-    if (dp[N][i] >= M)
+    if (dp[i] >= M)
     {
       cout << i << endl;
       break;
