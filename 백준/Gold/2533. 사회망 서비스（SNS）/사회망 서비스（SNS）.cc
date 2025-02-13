7a10fb43e884da719b42eac2d5c24034d7835158
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-14 01:03:23 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 856 ms, Memory: 113308 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -1,45 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#include<queue>
+#include <string>
+#include<math.h>
+#include<map>
+#define ll long long
+#define P 1000000007
+#define endl "\n"
 using namespace std;
 
-const int MAX_N = 1000001;
-vector<int> tree[MAX_N];
-int dp[MAX_N][2];  // dp[u][0]: 얼리 아답터 X, dp[u][1]: 얼리 아답터 O
-bool visited[MAX_N];
+vector<vector<int>> childNodes;
+vector<vector<int>> dp;
+vector<bool> visited;
 
-// DFS + DP
 void dfs(int node) {
-    visited[node] = true;
-    dp[node][0] = 0;  // 얼리 아답터 X
-    dp[node][1] = 1;  // 얼리 아답터 O
+  visited[node] = true;
+  dp[node][1] = 1;
+  dp[node][0] = 0;
 
-    for (int child : tree[node]) {
-        if (!visited[child]) {
-            dfs(child);
-            dp[node][0] += dp[child][1];  // 자식이 얼리 아답터여야 함
-            dp[node][1] += min(dp[child][0], dp[child][1]);  // 자식이 얼리 아답터일 수도 있고 아닐 수도 있음
-        }
+  for (auto c : childNodes[node]) {
+    if (!visited[c]) {
+      dfs(c);
+      dp[node][0] += dp[c][1];
+      dp[node][1] += min(dp[c][1], dp[c][0]);
     }
+  }
 }
 
 int main() {
-    int n;
-    cin >> n;
+  cin.tie(0);
+  cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  childNodes = vector<vector<int>>(N + 1, vector<int>());
+  visited = vector<bool>(N + 1, 0);
+  dp = vector<vector<int>>(N + 1, vector<int>(2, -1));
 
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        tree[u].push_back(v);
-        tree[v].push_back(u);
-    }
+  for (int i = 0; i < N-1;i++) {
+    int x, y;
+    cin >> x >> y;
+    childNodes[x].push_back(y);
+    childNodes[y].push_back(x);
+  }
 
-    memset(dp, -1, sizeof(dp));
-    memset(visited, false, sizeof(visited));
+  dfs(1);
+  cout << min(dp[1][0], dp[1][1]) << endl;
 
-    dfs(1);  // 1번 노드를 루트로 가정
-
-    cout << min(dp[1][0], dp[1][1]) << endl;
-    return 0;
+return 0;
 }
