97f029b74a01eaa54c944175953be1245195a0e7
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-14 16:52:05 +0900

    [Gold V] Title: 최소비용 구하기, Time: 32 ms, Memory: 4748 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -9,60 +9,49 @@
 #define ll long long
 #define P 1000000007
 #define endl "\n"
+#define INF 987654321
 using namespace std;
 int N, M;
 vector<int> cost;
 vector<vector<pair<int, int>>> adj;
 vector<bool> visited;
 
-typedef struct cmpCost {
-
-  bool operator()(pair<int, int> a, pair<int, int> b) {
-    return a.second > b.second;
-  }
-
-}cmpCost;
 
 int main() {
-cin.tie(0);
-cout.tie(0);
-ios_base::sync_with_stdio(false);
-cin >> N >> M;
+  cin.tie(0);
+  cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin >> N >> M;
 
-adj.resize(N + 1);
-cost = vector<int>(N + 1, INT32_MAX);
-visited = vector<bool>(N + 1, 0);
+  adj.resize(N + 1);
+  cost = vector<int>(N + 1, INT32_MAX);
 
-for (int i = 0; i < M;i++) {
-  int x, y, c;
-  cin >> x >> y >> c;
-  adj[x].push_back({ y,c });
-}
-
-int s, t;
-cin >> s >> t;
-priority_queue<pair<int, int>, vector<pair<int, int>>, cmpCost> q;
-q.push({ s, 0 });
-while(!q.empty()){
-  pair<int, int> curr = q.top();
-  q.pop();
-  if (visited[curr.first])  continue;
-  cost[curr.first] = curr.second;
-  visited[curr.first] = true;
-  for (pair<int, int> next : adj[curr.first]) {
-    if (visited[next.first])  continue;
-    q.push({ next.first, cost[curr.first] + next.second});
+  for (int i = 0; i < M;i++) {
+    int x, y, c;
+    cin >> x >> y >> c;
+    adj[x].push_back({ y,c });
   }
-}
 
-// for( int j =1 ; j<N+1 ;j++){
-//   cout << cost[j] << " ";
-// }
-// cout << endl;
-cout << cost[t] << endl;
+  int s, t;
+  cin >> s >> t;
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+  cost[s] = 0;
+  q.push({ 0, s });
+  while (!q.empty()) {
+    pair<int, int> curr = q.top();
+    q.pop();
+    if (cost[curr.second] < curr.first) continue;
+    for (pair<int, int> next : adj[curr.second]) {
+      if (curr.first + next.second >= cost[next.first]) continue;
+      cost[next.first] = curr.first + next.second;
+      q.push({ curr.first + next.second , next.first });
+    }
+  }
+
+  cout << cost[t] << endl;
 
 
 
 
-return 0;
+  return 0;
 }
