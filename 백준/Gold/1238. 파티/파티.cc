b935efa8a9e27a6839834186f2cc906b1842e2a4
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-02-14 14:14:24 +0900

    [Gold III] Title: 파티, Time: 148 ms, Memory: 2304 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.cc
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#include<queue>
+#include <string>
+#include<math.h>
+#include<map>
+#define ll long long
+#define P 1000000007
+#define endl "\n"
+using namespace std;
+int N, M;
+vector<int> cost;
+vector<vector<pair<int, int>>> adj;
+vector<bool> visited;
+
+
+typedef struct cmpCost {
+
+  bool operator()(pair<int, int> a, pair<int, int> b) {
+    return a.second > b.second;
+  }
+
+}cmpCost;
+
+
+int dijkstra(int st ,int en){
+  cost = vector<int>(N + 1, INT32_MAX);
+  visited = vector<bool>(N + 1, 0);
+  priority_queue<pair<int, int>, vector<pair<int, int>>, cmpCost> q;
+  q.push({ st, 0 });
+  while (!q.empty()) {
+    pair<int, int> curr = q.top();
+    q.pop();
+    if (visited[curr.first])  continue;
+    cost[curr.first] = curr.second;
+    visited[curr.first] = true;
+    for (pair<int, int> next : adj[curr.first]) {
+      if (visited[next.first])  continue;
+      if (curr.second + next.second > cost[next.first]) continue;
+      q.push({ next.first, curr.second + next.second });
+    }
+  }
+  return cost[en];
+}
+
+
+int main() {
+  cin.tie(0);
+  cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  int x;
+  cin >> N >> M >> x;
+  adj.resize(N + 1);
+  for (int i = 0; i < M; i++) {
+    int x, y, c;
+    cin >> x >> y >> c;
+    adj[x].push_back({y,c});
+  }
+  vector<int>sols(N+1,0);
+  for (int i = 1; i < N + 1; i++){
+    // cout << "dijkstra start" << endl;
+    sols[i] = dijkstra(i, x);
+  }
+
+  dijkstra(x, 0);
+
+  for (int i = 1; i < N + 1; i++) {
+    sols[i] += cost[i];
+  }
+
+  int ans = 0;
+  for (int i = 1; i < N + 1; i++) {
+    ans = max(ans, sols[i]);
+  }
+
+  cout << ans <<endl;
+
+
+  return 0;
+}
