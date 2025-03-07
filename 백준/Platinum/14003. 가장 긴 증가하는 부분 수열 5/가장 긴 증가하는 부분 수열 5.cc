9dfaf169991f93a9c47442890f52baad0b30d293
육세현 <139142064+sehyunsix@users.noreply.github.com>
2025-03-08 00:28:26 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 232 ms, Memory: 53176 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -15,102 +15,50 @@ using namespace std;
 int N;
 int A[10000001];
 int dp[1000001];
-int M[1000000];
+int parent[1000000];
 int main() {
 
-cin.tie(0);
-cout.tie(0);
-ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  ios_base::sync_with_stdio(false);
 
-cin >> N;
+  cin >> N;
 
-for( int i =0 ; i<N; i++){
-  cin >> A[i];
-}
-
-
-
-
-int fixed_start = 0;
-int max_lcs_len = 0;
-for (int i = 0; i < N; i++) {
-
-  // for (int i = 1; i < max_lcs_len + 1; i++) {
-  //   cout << dp[i] << " ";
-  // }
-  // cout << endl;
-
-  if (max_lcs_len == 0) {
-
-    max_lcs_len += 1;
-    dp[1] = 0;
-    dp[0] = -1;
-    M[0] = -1;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
   }
-  else {
 
+  int maxL = 0;
+  parent[0] = -1;
+  for (int i = 0; i < N; i++) {
     int mid;
-    int start = 1;
-    int end = max_lcs_len;
-    int target = A[i];
-
-    while (start <= end) {
-      mid = (start + end) / 2;
-        if (A[dp[mid]] >= target) {
-          end = mid;
-          if (start == end) {
-            break;
-          }
-        }
-        else{
-          start = mid + 1;
-        }
+    int st = 1;
+    int en = maxL + 1;
+    while (st < en) {
+      mid = (st + en) / 2;
+      if (A[i] <= A[dp[mid]]) en = mid;
+      else st = mid + 1;
     }
-    if (start == max_lcs_len + 1) {
-      max_lcs_len += 1;
-      M[i] = dp[max_lcs_len-1];
-      dp[max_lcs_len] = i;
-    }
-    else {
-      M[i] = dp[end-1];
-      dp[end] = i;
-    }
+    if(en ==maxL+1) maxL++;
+    dp[en] = i;
+    parent[i] =(en==1?-1:dp[en - 1]);
   }
 
-}
-int ans1 = 0;
-int ans2 = 0;
+  cout << maxL << endl;
 
-cout << max_lcs_len << endl;
+  int idx = dp[maxL];
+  stack<int> s;
+  while (idx > -1) {
+    s.push(A[idx]);
+    idx = parent[idx];
+  }
 
-int peak_idx = dp[max_lcs_len];
-// cout << peak_idx << endl;
-stack<int> s;
-int next_idx = peak_idx;
-while (next_idx > -1) {
-  // cout << next_idx<< endl;
-  s.push(A[next_idx]);
-  next_idx = M[next_idx];
-}
-// for (int j = 0; j < peak_idx;j++) {
-//   cout << A[dp[j]] << " ";
-// }
-// cout << endl;
+  while (!s.empty())
+  {
+    cout << s.top() << " ";
+    s.pop();
+  }
+  cout << endl;
 
-// for( int j =0 ; j<peak_idx+1 ;j++){
-//   cout << M[j] << " ";
-// }
-// cout << endl;
-
-while (!s.empty())
-{
-  cout << s.top() << " ";
-  s.pop();
-}
-cout << endl;
-
-
-
-
-return 0;
+  return 0;
 }
