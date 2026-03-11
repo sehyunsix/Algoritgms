f34b4bfa61c3d74b9b6138e31fff6860e4d4b64f
육세현 <139142064+sehyunsix@users.noreply.github.com>
2026-03-11 21:06:54 +0900

    [Platinum III] Title: 같은 탑, Time: 188 ms, Memory: 112960 KB -BaekjoonHub

--- 백준/Platinum/1126. 같은 탑/같은 탑.rs
+++ 백준/Platinum/1126. 같은 탑/같은 탑.rs
@@ -7,22 +7,18 @@ fn main() {
     let mut it = input.split_whitespace();
     let n : usize = it.next().unwrap().parse().unwrap();
     let mut array: Vec<usize> = it.take(n).map(|x| x.parse().unwrap()).collect();
-    let mut dp = vec![vec![0i32 ;500001];51];
+    let mut dp = vec![vec![-1i32 ;500001];51];
     array.sort();
     let s : usize = array.iter().sum();
     
-    for i in 1..n+1{
-        dp[i][array[i-1]] = array[i-1] as i32;
-    }
-    dp[1][array[0]] =array[0] as i32;
-    for i in 2..n+1 {
+    dp[0][0]=0;
+    for i in 1..n+1 {
         for k  in 0usize..s as usize+1 {
             if (k+array[i-1]) <= s  && (k+array[i-1]) <= 250000  {
                 dp[i][k] = dp[i][k].max(dp[i-1][k+array[i-1]]);
             }
             let diff :i32 = (array[i-1] as i32 -k as i32).abs();
-            // println!("i k diff dp[i][k]: {i} {k} {diff} {}",dp[i][k]);
-            if(dp[i-1][diff as usize]> 0){
+            if(dp[i-1][diff as usize]>= 0){
                 if (array[i-1] as i32 -k as i32)<0 {
                     dp[i][k]= dp[i][k].max(dp[i-1][diff as usize]+k as i32- diff );
                 }else{
@@ -30,16 +26,8 @@ fn main() {
                 }            
             }
             dp[i][k]= dp[i][k].max(dp[i-1][k]);
-            // println!("i k diff dp[i][k]: {i} {k} {diff} {}",dp[i][k]);
         }
     }
-    // println!("debug : {}", dp[1][2]);
-    // println!("debug : {}", dp[1][3]);
-    // println!("debug : {}", dp[2][1]);
-    // println!("debug : {}", dp[2][3]);
-    // println!("debug : {}", dp[2][2]);
-    // println!("debug : {}", dp[1][8]);
-    // println!("debug : {}", dp[2][5]);
 
     let mut sol : i32= 0;
     for i in 1..n+1 {
